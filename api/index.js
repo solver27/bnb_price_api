dea34d9007de66e636c35f1f24799e533b1f23a1
solver27 <m.leonel0507@gmail.com>
2024-06-27 21:15:33 -0400

    add /api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-let price;
+
 app.get("/", async (req, res) => {
   const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT";
   try {
@@ -12,9 +12,9 @@ app.get("/", async (req, res) => {
   } catch (err) {
     console.error("Unable to connect to Binance API", err);
   }
-  res.sendStatus(parseFloat(price.price))
+  res.sendStatus(parseFloat(price.price));
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
